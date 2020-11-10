ca5bd56589e939620fec2fbcc3917631335b3810
rolftb <rolftb@gmail.com>
2020-11-10 00:27:46 -0300

    probado y funcionando hasta #Build the model

--- R_code/R_alone/Rolf.R
+++ R_code/R_alone/Rolf.R
@@ -48,19 +48,38 @@ F_date<-function(data_X){
   data_X$V1 = NULL
   data_X$date = NULL
   data_X$shop_id=as.factor(data_X$shop_id)
-  data_X$item_category_id=as.factor(data_X$item_category_id)
-
-# mueve la columna de i tem_cnt_day
-data_X <- data_X[,c(2,1,3:1248)]
-
-return(data_X)
+    #data_X$item_category_id=as.factor(data_X$item_category_id)
+    #data_X$item_category_id=integer(data_X$item_category_id)
+  
+  # tengo que quitarle el formato de factor a item_category_id
+  
+  # mueve la columna de i tem_cnt_day
+  data_X <- data_X[,c(2,1,3:1282)]
+  
+  #return(data_X)
 }
 
-F_date(data_test)
-F_date(data_train)
+data_test<-F_date(data_test)
+data_train<-F_date(data_train)
 rm(F_date)
 
 ## Continuamos con las particiones ####
+
+### (No se deberia usar)Split the data into training and test set####
+# Esto no se deberia usar #
+set.seed(123)
+training.samples <- model_data$SalePrice %>%
+  createDataPartition(p = 0.8, list = FALSE)
+
+train.data  <- model_data[training.samples, ]
+test.data <- model_data[-training.samples, ]
+
+pred_data = model_data_aux[is.na(model_data_aux$SalePrice),] 
+pred_data$SalePrice = NULL
+##################################
+
+
+
 ### Predictor variables
 x <- model.matrix(item_cnt_day~., data_train)[,-1]
 
@@ -74,5 +93,47 @@ newX<- model.matrix(item_cnt_day~., data_test)[,-1]
 newY <- data_test$item_cnt_day
 
 
+## Reci?n ahora empecemos a resolver la prueba...####
 
+#revisar si tiene datos NA
+#checkDataQuality(data_train, 'DataQualityReport/num.csv', 'DataQualityReport/cat.csv')
+# No tiene ningun NA
+#data_train$item_category_id=as.integer(data_train$item_category_id)
+# tengo que quitarle el formato de factor a item_category_id
+
+# Preg 2. Regresion Lineal Multiple
+# a. Selecci?n Stepwise forward
+
+null<-lm((item_cnt_day)~1, data=data_train)
+full<-lm(item_cnt_day~., data=data_train)
+#error presente si existen NA o un factor con solo una categoria
+#esto sucede con item_category_id
+
+
+output2a<-step(null, scope = list(upper=full), data=data_train, direction="both")
+summary(output2a)
+model2a =  output2a$call$formula
+
+### Build the model
+set.seed(123)
+lm_model <- train(
+  item_cnt_day ~., data = data_train, method = "lm",
+  trControl = trainControl("cv", number = 10),
+  tuneLength = 10
+)
+### Model coefficients
+coef(lm_model$finalModel)
+
+
+### Make predictions
+predictions <- lm_model %>% predict(test.data)
+
+### Model prediction performance
+data.frame(
+  RMSE = RMSE(predictions, test.data$SalePrice),
+  Rsquare = R2(predictions, test.data$SalePrice)
+)
+
+varsSelected <- length(coef(lm_model$finalModel))
+cat('Ridge uses', varsSelected, 'variables in its model')
 
